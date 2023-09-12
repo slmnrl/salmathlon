fc6b30f61d22ca3fa26aa79383469657ba382ed6
slmnrl <salma.nurul21@ui.ac.id>
2023-09-12 21:44:05 +0700

    update

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Product
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,9 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    # Unit test apakah produk dapat dibuat dengan tepat
+    def test_product_creation(self):
+        s = Product.objects.create(name = "Skechers GOrun", amount = 10, description = "Advanced Cushioning Technology")
+        self.assertTrue(isinstance(s, Product))
+    
