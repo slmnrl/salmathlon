9d316ac72718d335594e8aa75fc128774417f951
slmnrl <salma.nurul21@ui.ac.id>
2023-11-22 08:49:57 +0700

    add some settings

--- salmathlon/settings.py
+++ salmathlon/settings.py
@@ -147,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://salma-nurul21-tugas.pbp.cs.ui.ac.id']
